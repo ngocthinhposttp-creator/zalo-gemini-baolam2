6755e23eadb8ef57148c41d78f97acbba798c006
ngocthinhposttp-creator <ngocthinhpost.tp@gmail.com>
2026-06-03 10:07:58 +0700

    app.py

--- app.py
+++ app.py
@@ -8,34 +8,30 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 @app.route("/")
 def home():
-    return "UBND xa Bao Lam 2 - Gemini API"
+    return "UBND xa Bao Lam 2 - Gemini OK"
 
-@app.route("/chat", methods=["POST"])
+@app.route("/chat")
 def chat():
 
-    data = request.json
-
-    question = data.get("message", "")
+    question = request.args.get("q","Xin chào")
 
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
 
     payload = {
-        "contents": [{
-            "parts": [{
-                "text": question
-            }]
-        }]
+        "contents":[
+            {
+                "parts":[
+                    {
+                        "text": question
+                    }
+                ]
+            }
+        ]
     }
 
-    r = requests.post(url, json=payload)
+    r = requests.post(url,json=payload)
 
-    result = r.json()
-
-    answer = result["candidates"][0]["content"]["parts"][0]["text"]
-
-    return jsonify({
-        "answer": answer
-    })
+    return r.json()
 
 if __name__ == "__main__":
     app.run()
